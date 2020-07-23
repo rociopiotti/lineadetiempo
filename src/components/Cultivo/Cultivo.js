d082fa5f9c354e5ad8c33c1364323ad2f3a82c42
Rocio <ropiotti@gmail.com>
2020-07-23 12:39:16 +0200

    Cultivo content

--- src/components/Cultivo/Cultivo.js
+++ src/components/Cultivo/Cultivo.js
@@ -4,13 +4,87 @@ import styled from "styled-components/macro";
 
 import { ContainerColumn } from "../../theme/Theme";
 
+const TextContainer = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: blue;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TextContent = styled.p`
+  width: 100%;
+  height: auto;
+  background-color: lightblue;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 2vw 2vh;
+  line-height: 1.3em;
+`;
+
+const ImgContainer = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: violet;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImgContent = styled.p`
+  width: 100%;
+  height: auto;
+  background-color: pink;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const VideoContainer = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: green;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const VideoContent = styled.p`
+  width: 100%;
+  height: auto;
+  background-color: lightgreen;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
 const Cultivo = () => {
   return (
     <ContainerColumn theme={{ testingBGColor: "lightblue" }}>
-      <p>CULTIVO</p>
-      <p>CULTIVO</p>
-      <p>CULTIVO</p>
-      <p>CULTIVO</p>
+      <TextContainer>
+        <TextContent>
+          Et consectetur Lorem culpa nisi ex duis pariatur velit. Pariatur nulla
+          eu amet exercitation et officia voluptate ullamco elit pariatur in
+          ullamco. Aute minim ea quis ad ipsum aliquip amet dolore amet id
+          aliqua. Id fugiat commodo exercitation velit enim. Qui sit dolor elit
+          et amet eu nostrud nisi Lorem tempor eiusmod veniam. Id officia amet
+          incididunt aliqua ut.
+        </TextContent>
+      </TextContainer>
+      <ImgContainer>
+        <ImgContent>IMG</ImgContent>
+      </ImgContainer>
+      <VideoContainer>
+        <VideoContent>VIDEO</VideoContent>
+      </VideoContainer>
     </ContainerColumn>
   );
 };
